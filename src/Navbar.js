cd59289cccd9bcff05102eba3e6bc17ba7ee08cc
Maik de Kruif <maik@maik.dev>
2020-04-19 18:12:34 +0200

    Added a fake login state

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,22 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+const Auth = {
+  isAuthenticated: true,
+  signIn(email, password, cb) {
+    Auth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signOut(cb) {
+    Auth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+const account = {
+  name: "Maik de Kruif",
+};
+
 export default function Routes() {
   return (
     <Navbar id="Navbar">
@@ -10,8 +26,22 @@ export default function Routes() {
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>Hello</Navbar.Text>
+        <Navbar.Text>
+          <ProfileText />
+        </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
+  );
+}
+
+function ProfileText() {
+  return Auth.isAuthenticated ? (
+    <p>
+      Signed in as: <Link to="/profile">{account.name}</Link>
+    </p>
+  ) : (
+    <p>
+      <Link to="/login">Log in</Link>
+    </p>
   );
 }
