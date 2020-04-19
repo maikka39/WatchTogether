82e9b23d2f918d3d1fac53b3a6f1bb19db8b2df1
Maik de Kruif <maik@maik.dev>
2020-04-19 18:48:47 +0200

    Remove App test

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders watch-together title link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/watch-together/i);
-  expect(linkElement).toBeInTheDocument();
-});
